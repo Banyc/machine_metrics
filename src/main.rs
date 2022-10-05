4a30e31e4fdfe399acbea6d6751414127ecf7c35
Banyc <36535895+Banyc@users.noreply.github.com>
2022-10-06 00:14:54 +0800

    feat: config file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-use std::{collections::HashSet, net::SocketAddr, sync::Arc, time};
+use std::{collections::HashSet, fs::File, io::Read, net::SocketAddr, sync::Arc, time};
 
 use axum::{middleware, routing::post, Json, Router};
 use log::info;
 use machine_metrics::{api, metrics, MetricCache};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
     env_logger::try_init().unwrap();
 
-    let shard_count = 4;
-    let ring_size = 1024;
-    let cache = MetricCache::new(shard_count, ring_size);
+    let config_path = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "config/config.toml".to_string());
+    let config: Config = {
+        let mut config_file = File::open(config_path).unwrap();
+        let mut config = String::new();
+        config_file.read_to_string(&mut config).unwrap();
+        toml::from_str(&config).unwrap()
+    };
+
+    let cache = MetricCache::new(config.shard_count, config.ring_size);
     let cache = Arc::new(cache);
 
-    let ethernet_name = "en0";
-    let api_tokens = vec!["unsafe".to_string()];
-
-    let api_tokens = HashSet::from_iter(api_tokens);
+    let api_tokens = HashSet::from_iter(config.api_tokens.iter().map(|x| x.token.clone()));
     let api_tokens = Arc::new(api_tokens);
 
     let mut sys_info = metrics::get_new_sys_info();
@@ -24,7 +30,7 @@ async fn main() {
         let cache = Arc::clone(&cache);
         tokio::spawn(async move {
             loop {
-                metrics::sample_sys_info(&cache, &mut sys_info, &ethernet_name);
+                metrics::sample_sys_info(&cache, &mut sys_info, &config.ethernet_name);
                 tokio::time::sleep(time::Duration::from_secs(5)).await;
             }
         });
@@ -50,4 +56,18 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiToken {
+    pub token: String,
+    pub description: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub shard_count: usize,
+    pub ring_size: usize,
+    pub ethernet_name: String,
+    pub api_tokens: Vec<ApiToken>,
 }
